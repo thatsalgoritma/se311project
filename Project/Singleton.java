b0c3733f781a5f7c58800951375d63aeaa9cbb7e
murat vermez <105073354+Muratvermez@users.noreply.github.com>
2025-05-10 16:39:02 +0300

    Update Singleton.java

--- Project/Singleton.java
+++ Project/Singleton.java
@@ -3,16 +3,19 @@ package Project;
 import java.util.HashMap;
 import java.util.Map;
 
+// Singleton class to store cattle location data globally
 class FarmDatabase {
 
     private static FarmDatabase instance;
 
-    private Map<String, Integer> cattleLocationData;
+    private Map<String, Integer> cattleLocationData;  // holds the cattle IDs and their locations
 
+    // private constructor
     private FarmDatabase() {
         cattleLocationData = new HashMap<>();
     }
 
+    // provides global access point to the single instance
     public static synchronized FarmDatabase getInstance() {
         if (instance == null) {
             instance = new FarmDatabase();
@@ -20,11 +23,13 @@ class FarmDatabase {
         return instance;
     }
 
+    // updates cattle location 
     public void updateLocation(String cattleId, Integer location) {
         cattleLocationData.put(cattleId, location);
-        System.out.println( cattleId + " is " + location + " meters away from farm!");
+        System.out.println(cattleId + " is " + location + " meters away from farm!");
     }
 
+    // gets location of a specific cattle
     public Integer getLocation(String cattleId) {
         return cattleLocationData.get(cattleId);
     }
